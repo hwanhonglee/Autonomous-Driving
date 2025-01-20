9495eff5de943744e5ba571388fb2740f241fc30
Hwanhong Lee <hwanhong57@gmail.com>
2025-01-20 10:52:33 +0900

    Initial commit

---
+++ universe/autoware.universe/planning/behavior_path_planner/include/behavior_path_planner/data_manager.hpp
@@ -0,0 +1,98 @@
+// Copyright 2021 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef BEHAVIOR_PATH_PLANNER__DATA_MANAGER_HPP_
+#define BEHAVIOR_PATH_PLANNER__DATA_MANAGER_HPP_
+
+#include "behavior_path_planner/parameters.hpp"
+#include "behavior_path_planner/util/drivable_area_expansion/parameters.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+#include <route_handler/route_handler.hpp>
+
+#include <autoware_auto_perception_msgs/msg/predicted_objects.hpp>
+#include <autoware_auto_planning_msgs/msg/path_with_lane_id.hpp>
+#include <geometry_msgs/msg/accel_with_covariance_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <lanelet2_core/geometry/Lanelet.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace behavior_path_planner
+{
+using autoware_auto_perception_msgs::msg::PredictedObjects;
+using autoware_auto_planning_msgs::msg::PathWithLaneId;
+using geometry_msgs::msg::AccelWithCovarianceStamped;
+using geometry_msgs::msg::PoseStamped;
+using nav_msgs::msg::OccupancyGrid;
+using nav_msgs::msg::Odometry;
+using route_handler::RouteHandler;
+struct BoolStamped
+{
+  explicit BoolStamped(bool in_data) : data(in_data) {}
+  bool data{false};
+  rclcpp::Time stamp{0, 0, RCL_ROS_TIME};
+};
+
+struct ModuleNameStamped
+{
+  std::string module_name = "NONE";
+  rclcpp::Time stamp{0, 0, RCL_ROS_TIME};
+};
+
+struct DrivableLanes
+{
+  lanelet::ConstLanelet right_lane;
+  lanelet::ConstLanelet left_lane;
+  lanelet::ConstLanelets middle_lanes;
+};
+
+struct PlannerData
+{
+  Odometry::ConstSharedPtr self_odometry{};
+  AccelWithCovarianceStamped::ConstSharedPtr self_acceleration{};
+  PredictedObjects::ConstSharedPtr dynamic_object{};
+  OccupancyGrid::ConstSharedPtr occupancy_grid{};
+  PathWithLaneId::SharedPtr reference_path{std::make_shared<PathWithLaneId>()};
+  PathWithLaneId::SharedPtr prev_output_path{std::make_shared<PathWithLaneId>()};
+  lanelet::ConstLanelets current_lanes{};
+  std::shared_ptr<RouteHandler> route_handler{std::make_shared<RouteHandler>()};
+  BehaviorPathPlannerParameters parameters{};
+  drivable_area_expansion::DrivableAreaExpansionParameters drivable_area_expansion_parameters{};
+
+  template <class T>
+  size_t findEgoIndex(const std::vector<T> & points) const
+  {
+    return motion_utils::findFirstNearestIndexWithSoftConstraints(
+      points, self_odometry->pose.pose, parameters.ego_nearest_dist_threshold,
+      parameters.ego_nearest_yaw_threshold);
+  }
+
+  template <class T>
+  size_t findEgoSegmentIndex(const std::vector<T> & points) const
+  {
+    return motion_utils::findFirstNearestSegmentIndexWithSoftConstraints(
+      points, self_odometry->pose.pose, parameters.ego_nearest_dist_threshold,
+      parameters.ego_nearest_yaw_threshold);
+  }
+};
+
+}  // namespace behavior_path_planner
+
+#endif  // BEHAVIOR_PATH_PLANNER__DATA_MANAGER_HPP_

d7ae3d97d2e4bcb248bb0996a92986f080be4fb2
Hwanhong Lee <hwanhong57@gmail.com>
2025-01-20 16:37:31 +0900

    Initial commit

---
+++ novatel_gps/novatel_gps_driver/launch/tester_for_usb.launch.py
@@ -0,0 +1,34 @@
+"""Launch an example driver that communicates using USB"""
+
+from launch import LaunchDescription
+import launch_ros.actions
+
+
+def generate_launch_description():
+    container = launch_ros.actions.ComposableNodeContainer(
+        name='novatel_gps_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            launch_ros.descriptions.ComposableNode(
+                package='novatel_gps_driver',
+                plugin='novatel_gps_driver::NovatelGpsNode',
+                name='novatel_gps',
+                parameters=[{
+                    'connection_type': 'serial',
+                    'device': '/dev/ttyUSB1', # HH_231108 for usb port
+                    'verbose': True,
+                    'publish_novatel_positions': True,
+                    'publish_novatel_psrdop2': True,
+                    'publish_novatel_velocity': True,
+                    'publish_novatel_dual_antenna_heading': True, # HH_230414
+                    'publish_novatel_heading2': True, # HH_230414
+                    'frame_id': 'gps'
+                }]
+            )
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([container])
